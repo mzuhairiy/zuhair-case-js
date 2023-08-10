5face6b491325169d5d46c564574f6d46dccab5c
mzuhairiy <atasnamazuhair@gmail.com>
2023-08-10 14:05:50 +0700

    new file:   soal1.js

---
+++ soal1.js
@@ -0,0 +1,17 @@
+function akarPangkat() {
+    const prompt = require('prompt-sync')({sigint: true});
+
+    let x = prompt("Masukkan bilangan genap positif:");
+  
+    if (x < 0) {
+      console.log("Tidak bisa input bilangan negatif");
+    } else if (x % 2 !== 0) {
+      console.log("Tidak bisa input bilangan ganjil");
+    } else {
+      let result = Math.sqrt(x);
+      console.log(`Akar pangkat 2 dari ${x} adalah ${result}`);
+    }
+  }
+  
+  akarPangkat();
+  
