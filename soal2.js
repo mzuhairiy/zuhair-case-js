c3a33a83d86ebc9853ffa243b7641978e611e6d3
mzuhairiy <atasnamazuhair@gmail.com>
2023-08-10 16:27:49 +0700

    add new file product.json and implement DDT

--- soal2.js
+++ soal2.js
@@ -1,39 +1,7 @@
-let productBin = {
-    "requestId": null,
-    "data": [
-        {
-            "id": 100000057465,
-            "storageId": 10000008207,
-            "code": "A01-01-01-A",
-            "productId": 110000081009,
-            "productName": "FloBrand-DressBSPink",
-            "productCode": "FBR00040101",
-            "quantity": 76,
-            "createdTime": "2021-12-21T13:54:48Z",
-          },
-          {
-            "id": 100000057466,
-            "storageId": 10000002181,
-            "code": "A01-01-01-B",
-            "productId": 110000081009,
-            "productName": "FloBrand-DressBSPink",
-            "productCode": "FBR00040101",
-            "quantity": 71,
-            "createdTime": "2021-12-21T13:54:48Z",
-          },
-          {
-            "id": 100000065224,
-            "storageId": 10000008884,
-            "code": "Tgt00-A-A-01",
-            "productId": 110000081009,
-            "productName": "FloBrand-DressBSPink",
-            "productCode": "FBR00040101",
-            "quantity": 10,
-            "createdTime": "2022-02-08T10:35:19Z",
-          }
-    ],
-    "message": "success"
-  };
+const productData = require('../zuhair-sanber-1/data/product.json');
+
+let productBin = { productData 
+};
   
   const productCode = "FBR00040101";
   
@@ -41,10 +9,10 @@ let productBin = {
   const quantityStorageId = {};
   
   // Mengisi objek quantityStorageId dengan quantity dari setiap storageId
-  productBin.data.forEach(product => {
-    if (product.productCode === productCode) {
-      const storageId = product.storageId;
-      const quantity = product.quantity;
+  productBin.productData.data.forEach(productData => {
+    if (productData.productCode === productCode) {
+      const storageId = productData.storageId;
+      const quantity = productData.quantity;
   
       if (quantityStorageId[storageId] === undefined) {
         quantityStorageId[storageId] = quantity;
